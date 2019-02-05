3b546d14f4f2e31637e7223f7c56f6a5377ae4f8
[Yashwanth Banda] <bandayashwanth@gmail.com>
2019-02-04 20:48:38 -0600

    initial commit

---
+++ src/main/java/com/ds/algos/codinstruct/CodinstructApplication.java
@@ -0,0 +1,14 @@
+package com.ds.algos.codinstruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodinstructApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodinstructApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/ds/algos/codinstruct/CodinstructApplicationTests.java
@@ -0,0 +1,17 @@
+package com.ds.algos.codinstruct;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CodinstructApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
